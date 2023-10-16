b6b80a62101934af1b04e8109f5406546f4b4ccf
SUHAIB SABIR <suhaibsabir110@gmail.com>
2023-10-15 19:03:18 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var a
+
+a = prompt("entre the color")
+
+
+if (a == "red") {
+    alert("yellow")
+}
+
+else if (a == "yellow") {
+
+    alert("green")
+
+}
+
+else if (a == "green") {
+
+    alert("lights off now you can go")
+
+}
+
+else {
+    alert("sinal is off")
+}
